8b9fcb4e61d378807e93ee3ef02776eec69c0e46
Chris Marshall <chris@codegoalie.com>
2021-09-14 08:30:57 -0400

    Don't fail WDWNTunes time parse error
    They have inconsistent timestamp formats

--- wdwnt/tunes.go
+++ wdwnt/tunes.go
@@ -42,7 +42,7 @@ func (t Tunes) ParseTrackInfo(raw []byte) (*models.TrackInfo, error) {
 	startedAt, err := time.Parse("2006-01-02 15:04:05-07:00", resp.CurrentTrack.Start)
 	if err != nil {
 		err = fmt.Errorf("failed to parse WDWNTunes started at info: %w", err)
-		return nil, err
+		startedAt = time.Time{}
 	}
 
 	info.Title = resp.CurrentTrack.Title
